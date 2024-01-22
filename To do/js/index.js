210a8489c02322f4c6adc991e55b886d0ef2821b
Gustavo Melo <gustamvieira@gmail.com>
2024-01-22 14:18:32 -0300

    correcting the js file

--- To do/js/index.js
+++ To do/js/index.js
@@ -5,131 +5,246 @@ const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const searchInput = document.querySelector("#search-input");
+const eraseBtn = document.querySelector("#erase-button");
+const filterBtn = document.querySelector("#filter-select");
 
 let oldInputValue;
 
-
 // Funções
+const saveTodo = (text, done = 0, save = 1) => {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
 
-// const function = () => {} eh outra forma de sintaxe de uma funcao
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
 
-const saveTodo = (text) => {
+  const doneBtn = document.createElement("button");
+  doneBtn.classList.add("finish-todo");
+  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  todo.appendChild(doneBtn);
 
-    const todo = document.createElement("div");
-    todo.classList.add("todo");
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-todo");
+  editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+  todo.appendChild(editBtn);
 
-    const todoTitle = document.createElement("h3");
-    todoTitle.innerText = text;
-    todo.appendChild(todoTitle);
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("remove-todo");
+  deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  todo.appendChild(deleteBtn);
 
-    const doneBtn = document.createElement("button");
-    doneBtn.classList.add("finish-todo");
-    //Incorporar um elemento dentro de outro
-    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
-    todo.appendChild(doneBtn);
-    
-    const editBtn = document.createElement("button");
-    editBtn.classList.add("edit-todo");
-    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
-    todo.appendChild(editBtn);
+  // Utilizando dados da localStorage
+  if (done) {
+    todo.classList.add("done");
+  }
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("remove-todo");
-    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-    todo.appendChild(deleteBtn);
-    
-    //incrementar o item da lista na lista geral
-    todoList.appendChild(todo);
+  if (save) {
+    saveTodoLocalStorage({ text, done: 0 });
+  }
 
-    todoInput.value = "";
-    // Para facilitar que o usuario ja comece digitando outra tarefa
-    todoInput.focus();
+  todoList.appendChild(todo);
+
+  todoInput.value = "";
 };
 
 const toggleForms = () => {
-    editForm.classList.toggle("hide");
-    todoForm.classList.toggle("hide");
-    todoList.classList.toggle("hide");
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
 };
 
 const updateTodo = (text) => {
-    //basicamente um array de todos 
-    const todos = document.querySelectorAll("todo");
+  const todos = document.querySelectorAll(".todo");
 
-    todos.forEach((todo) => {
-        //primeiro seleciona o elemento e depois seleciona a propriedade desejada
-        let todoTitle = todo.querySelector("h3");
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector("h3");
 
-        //se tiver certo, altera o texto
-        if(todoTitle.innerText === oldInputValue) {
-            todoTitle.innerText = text;
-        }
-    });
+    if (todoTitle.innerText === oldInputValue) {
+      todoTitle.innerText = text;
+
+      // Utilizando dados da localStorage
+      updateTodoLocalStorage(oldInputValue, text);
+    }
+  });
+};
+
+const getSearchedTodos = (search) => {
+  const todos = document.querySelectorAll(".todo");
+
+  todos.forEach((todo) => {
+    const todoTitle = todo.querySelector("h3").innerText.toLowerCase();
+
+    todo.style.display = "flex";
+
+    console.log(todoTitle);
+
+    if (!todoTitle.includes(search)) {
+      todo.style.display = "none";
+    }
+  });
+};
+
+const filterTodos = (filterValue) => {
+  const todos = document.querySelectorAll(".todo");
+
+  switch (filterValue) {
+    case "all":
+      todos.forEach((todo) => (todo.style.display = "flex"));
+
+      break;
+
+    case "done":
+      todos.forEach((todo) =>
+        todo.classList.contains("done")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none")
+      );
+
+      break;
+
+    case "todo":
+      todos.forEach((todo) =>
+        !todo.classList.contains("done")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none")
+      );
+
+      break;
+
+    default:
+      break;
+  }
 };
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const inputValue = todoInput.value;
+  const inputValue = todoInput.value;
 
-    if(inputValue) {
-        saveTodo(inputValue);
-    }
+  if (inputValue) {
+    saveTodo(inputValue);
+  }
 });
 
-// evento para os cliques dos botoes finish, edit e remove
 document.addEventListener("click", (e) => {
-    
-    const targetEl = e.target;
-    //para que o programa identifique que o botao foi apertado do elemento da lista mais proximo, que eh o seu "pai"
-    const parentEl = targetEl.closest("div");
-    //vamos usar o titulo como forma de identificar qual elemento da lista sera editado
-    let todoTitle;
+  const targetEl = e.target;
+  const parentEl = targetEl.closest("div");
+  let todoTitle;
 
-    if(parentEl && parentEl.querySelector("h3")) {
-        //o todoTitle recebe texto do titulo do elemento pai
-        todoTitle = parentEl.querySelector("h3").innerHTML;
-    }
+  if (parentEl && parentEl.querySelector("h3")) {
+    todoTitle = parentEl.querySelector("h3").innerText || "";
+  }
 
-    if(targetEl.classList.contains("finish-todo")) {
-        //incrementar a classe done, com todos os seus atributos de cor e tal no elemento que esta done
-        parentEl.classList.toggle("done");
-    }
-    
-    if(targetEl.classList.contains("edit-todo")) {
-        /* O que fazer:
-        1- criar uma funcao para esconder o formulario atual e exibir um de edicao
-        
-        */
+  if (targetEl.classList.contains("finish-todo")) {
+    parentEl.classList.toggle("done");
 
-        toggleForms();
+    updateTodoStatusLocalStorage(todoTitle);
+  }
 
-        //mudo o valor do input do edit
-        editInput.value = todoTitle;
-        //salvar o valor anterior para consulta e alteracao
-        oldInputValue = todoTitle;
-    }
-    
-    if(targetEl.classList.contains("remove-todo")) {
-        parentEl.remove()
-    }
+  if (targetEl.classList.contains("remove-todo")) {
+    parentEl.remove();
+
+    // Utilizando dados da localStorage
+    removeTodoLocalStorage(todoTitle);
+  }
+
+  if (targetEl.classList.contains("edit-todo")) {
+    toggleForms();
+
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
+  }
 });
 
 cancelEditBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    toggleForms();
+  e.preventDefault();
+  toggleForms();
 });
 
-editForm.addEventListener("click", (e) => {
-    e.preventDefault();
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const editInputvalue = editInput.value;
+  const editInputValue = editInput.value;
 
-    if(editInputvalue) {
-       updateTodo(editInputvalue); //atualizar
-    }
+  if (editInputValue) {
+    updateTodo(editInputValue);
+  }
 
-    toggleForms()
+  toggleForms();
 });
+
+searchInput.addEventListener("keyup", (e) => {
+  const search = e.target.value;
+
+  getSearchedTodos(search);
+});
+
+eraseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  searchInput.value = "";
+
+  searchInput.dispatchEvent(new Event("keyup"));
+});
+
+filterBtn.addEventListener("change", (e) => {
+  const filterValue = e.target.value;
+
+  filterTodos(filterValue);
+});
+
+// Local Storage
+const getTodosLocalStorage = () => {
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  return todos;
+};
+
+const loadTodos = () => {
+  const todos = getTodosLocalStorage();
+
+  todos.forEach((todo) => {
+    saveTodo(todo.text, todo.done, 0);
+  });
+};
+
+const saveTodoLocalStorage = (todo) => {
+  const todos = getTodosLocalStorage();
+
+  todos.push(todo);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const removeTodoLocalStorage = (todoText) => {
+  const todos = getTodosLocalStorage();
+
+  const filteredTodos = todos.filter((todo) => todo.text != todoText);
+
+  localStorage.setItem("todos", JSON.stringify(filteredTodos));
+};
+
+const updateTodoStatusLocalStorage = (todoText) => {
+  const todos = getTodosLocalStorage();
+
+  todos.map((todo) =>
+    todo.text === todoText ? (todo.done = !todo.done) : null
+  );
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const updateTodoLocalStorage = (todoOldText, todoNewText) => {
+  const todos = getTodosLocalStorage();
+
+  todos.map((todo) =>
+    todo.text === todoOldText ? (todo.text = todoNewText) : null
+  );
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+loadTodos();
